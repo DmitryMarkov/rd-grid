bdc38e53bc09e2c35ad57b4cd6dfd1117e1556a9
DmitryMarkov <forall@stylive.com>
2018-10-27 15:38:12 +0200

    integration check

---
+++ integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Core } from "@rd-grid/core";
+import { Plugins } from "@rd-grid/plugins";
+import { Renderers } from "@rd-grid/renderers";
+
+console.log(new Core());
+console.log(new Plugins());
+console.log(new Renderers());
